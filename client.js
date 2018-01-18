79ef4d4a99bcfaeb74fc0dda990228bbb71704ec
JackDunne <8jdunne@stpatrick.org>
2018-01-18 14:40:43 -0600

    Add files via upload

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 //2nd dash of readme
+const sort = Require('./Sort.js')
 function findStates(array){
     let states = [];
     for(let x = 0;x<array.length;x++){
@@ -15,10 +16,24 @@ function findStates(array){
 function percentDifference(array, percent){
     let statePercents = [];
     for(let x = 0;x<array.length;x++){
-        let temp = array[x].obamaPercent - array[x].romneyPercent;
-        temp = Math.abs(TEMP)//that is not correct
-        if(temp < percent){
-            //then you do things
+        if(array[x].fips == 0){
+          let temp = array[x].obamaPercent - array[x].romneyPercent;
+          temp = Math.abs(temp);
+          if(temp < percent){
+              statePercents.push(array[x].state);
+          }
         }
     }
+    return statePercents;
+}
+
+//4th dash of readme
+function totalVotes(state){
+    Sort.insertionSort(list,property)
+}
+
+//5th dash of readme
+function statesWon(array, candidate){
+  let won = [];
+  let number = array.obamaVote - array.romneyVote;
 }
